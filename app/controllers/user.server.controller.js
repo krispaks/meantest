d07e23b686c6db1abf56139b2d7236992d1d1075
krispaks <krispaks@yahoo.com>
2015-08-27 16:18:01 -0500

    doing CRUD in node js

--- app/controllers/user.server.controller.js
+++ app/controllers/user.server.controller.js
@@ -138,6 +138,16 @@ exports.signout = function(req, res){
 	res.redirect('/');
 }
 
+exports.requiresLogin = function(req, res, next){
+	if(!req.isAuthenticated()){
+		return res.status(401).send({
+			message: 'User is not logged in'
+		});
+	}
+	
+	next();
+};
+
 // OAuth
 exports.saveOAuthUserProfile = function(req, profile, done){
 	User.findOne({
