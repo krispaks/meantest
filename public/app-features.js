b0e6f5c4bfba0c297fac3fdf301c50b62c2e548b
Khris Allan Paca <krispaks@yahoo.com>
2015-09-08 10:22:29 -0500

    minor push
    minor push

--- public/app-features.js
+++ public/app-features.js
@@ -8,11 +8,16 @@ define(['require'
 	, 'articles/articles.client.controller'	
 	], function(requirejs, angular){
 		'use strict';
+		
+		// NOTE: the above setup where it will load all the files causes failure at times. 
+		// probably because files are loading asynchronously. need to find a way to organize them properly.
+		// I've tried below where in i required all other files except the 2 primary modules but doesnt seem to work.
+		// looks like the other controllers, routes and services are not attached.
 				
 		/*, 'user/authentication.client.services'
-	, 'articles/articles.client.routes'
-	, 'articles/articles.client.service'
-	, 'articles/articles.client.controller'*/
+		, 'articles/articles.client.routes'
+		, 'articles/articles.client.service'
+		, 'articles/articles.client.controller'*/
 		
 		/*requirejs(['user/authentication.client.services'
 			, 'articles/articles.client.routes'
@@ -22,8 +27,7 @@ define(['require'
 				return angular.module('appFeatures', ['users', 'articles']);
 		});*/
 		
-		//var appFeatureModule = angular.module('appFeatures', ['ngRoute','ngResource', 'users', 'articles']);
-		
+		//var appFeatureModule = angular.module('appFeatures', ['ngRoute','ngResource', 'users', 'articles']);		
 		
 		//return appFeatureModule;
 		
