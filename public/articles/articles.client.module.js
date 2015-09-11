2fcdfa82bb52c88c9f437f52cd2e9b79d092a744
krispaks <krispaks@yahoo.com>
2015-09-11 11:41:47 -0500

    minor update

--- public/articles/articles.client.module.js
+++ public/articles/articles.client.module.js
@@ -1,4 +1,18 @@
-define(['angular'], function(angular){
+define(['angular', 'require'], function(angular, requirejs){
 	'use strict';
-	return angular.module('articles', []);
+	
+	var articleModule = angular.module('articles', []);
+	
+	/*var articleRoute = requirejs('./articles.client.routes');
+	var articleService = requirejs('./articles.client.service');
+	var articleController = requirejs('./articles.client.controller');*/
+	
+	/*requirejs(['./articles.client.routes'
+			, './articles.client.service'
+			, './articles.client.controller'], 
+			function(){
+			
+		});*/
+	
+	return articleModule;
 });
